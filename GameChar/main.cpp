140d4c675d12f14d285c5d5ac42396138ef4be5f
bpang <brian.pang.9876@gmail.com>
2015-04-23 07:35:30 -0700

    Initial Commit

---
+++ GameChar/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameChar
+//
+//  Created by Brian Pang on 4/23/15.
+//  Copyright (c) 2015 Brian Pang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
